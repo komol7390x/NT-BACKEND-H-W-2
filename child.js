a2e6df30035125ddb2632fb647c9b32bdccff4bd
komol7390x <www.komol8689@gmail.com>
2025-06-18 17:06:45 +0500

    up

--- child.js
+++ child.js
@@ -1,5 +1,27 @@
-function sleep() {
-    console.log('Dasturning holati: Sleep!');
+function sleep(ms=1000) {
+    return new Promise(res => setTimeout(() => {
+        res(console.log(`Natija: Kod ${ms} ms ishga tushdi`)
+        )
+    },ms))
 }
+function random(start, stop) {
+    return Math.floor(Math.random()*stop+start)
+}
+class Person{
+    constructor(name, age) {
+        this.name = name,
+        this.age=age
+    }
+    get fullName() {
+        return this.name
+    }
+    get yoshi() {
+        return this.age
+    }
+    getInfo() {
+        console.log(`Ismi: ${this.fullName}\nYoshi: ${this.yoshi}`)
+    }
+}
+const person=new Person()
 
-module.exports={sleep}
+module.exports={sleep,random,Person}
--- scripts.js
+++ scripts.js
@@ -1,2 +1,28 @@
-const { sleep } = require("./child");
-sleep();
+const { sleep, random, Person } = require("./child");
+async function res() {
+    // -------------------------------------------
+    // Task-1
+    // await sleep(2000);
+    // -------------------------------------------
+    // Task-2
+    // console.log(random(100,999));
+    // -------------------------------------------
+    // Task-3
+    const info = new Person('Komol', 27);
+    // info.getInfo()
+    // -------------------------------------------
+    // Task-4
+    await sleep(random(100, 1000))
+    info.getInfo()
+}
+res()
+
+
+
+
+
+
+// console.log(random(100,999));
+const info = new Person('Komol', 27);
+// info.getInfo()
+
