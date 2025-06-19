11d22a45e0ed81fcc04f3882950a87f6eff03a64
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-19 16:51:45 +0500

    up

---
+++ CommonJS/main-1.js
@@ -0,0 +1,3 @@
+const {add,subtract}=require('./math');
+console.log("Qoshish amali: ",add(3,4));
+console.log("Ayrish amali: ",subtract(3,4));
---
+++ ESmodule/main-2.js
@@ -0,0 +1,4 @@
+import {add,subtract} from './math-2.js';
+console.log("Yigindi: ",add(3,2));
+console.log("Ayirma: ",subtract(3,2));
+
