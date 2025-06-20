264b33271e818ef713171c9c156c6ca58cbf6490
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-20 23:12:02 +0500

    up

--- treasure_map.js
+++ treasure_map.js
@@ -9,39 +9,48 @@ const obj = {
     hidden_temple: { 'treasure.txt': "Tabriklaymiz! Siz xazinani topdingiz!" }
 }
 function createFolder() {
-    if (!fs.existsSync(treaFolder)) {
-        fs.mkdirSync(treaFolder)
-        console.log(treaFolder, " papka yaratildi");
-    }
-
-    for (let [key, val] of Object.entries(obj)) {
-        const folderPath = path.join(treaFolder, key);
-        fs.mkdirSync(folderPath, { recursive: true });
-
-        for (let [file, text] of Object.entries(val)) {
-            const lastFolder = path.join(folderPath, file);
-            fs.writeFileSync(lastFolder, text, 'utf-8');
+    try {
+        if (!fs.existsSync(treaFolder)) {
+            fs.mkdirSync(treaFolder)
+            console.log(treaFolder, " papka yaratildi");
         }
+
+        for (let [key, val] of Object.entries(obj)) {
+            const folderPath = path.join(treaFolder, key);
+            fs.mkdirSync(folderPath, { recursive: true });
+
+            for (let [file, text] of Object.entries(val)) {
+                const lastFolder = path.join(folderPath, file);
+                fs.writeFileSync(lastFolder, text, 'utf-8');
+            }
+        }
+        console.log('Papkalar tayor');
+    } catch (error) {
+        console.log("Xatolik bor!", error.message);
     }
-    console.log('Papkalar tayor');
+
 }
 
 function readFolder() {
-    const folder = fs.readdirSync(treaFolder);
+    try {
+        const folder = fs.readdirSync(treaFolder);
 
-    folder.forEach(item => {
-        const folderPath = path.join(treaFolder, item);
-        if (fs.lstatSync(folderPath).isDirectory()) {
-            const files = fs.readdirSync(folderPath)
-            files.forEach(value => {
-                const filePath = path.join(folderPath, value);
-                const read = fs.readFileSync(filePath, 'utf-8');
-                console.log(`Papka nomi: ${item}\nFile nomi: ${files}\nXabar: ${read}\n--------------------`);
-            })
-        }
-    })
+        folder.forEach(item => {
+            const folderPath = path.join(treaFolder, item);
+            if (fs.lstatSync(folderPath).isDirectory()) {
+                const files = fs.readdirSync(folderPath)
+                files.forEach(value => {
+                    const filePath = path.join(folderPath, value);
+                    const read = fs.readFileSync(filePath, 'utf-8');
+                    console.log(`Papka nomi: ${item}\nFile nomi: ${files}\nXabar: ${read}\n--------------------`);
+                })
+            }
+        })
+    } catch (error) {
+        console.log("Xatolik bor!", error.message);
+    }
 }
 
 
-createFolder()
-readFolder()
+// createFolder()
+// readFolder()
