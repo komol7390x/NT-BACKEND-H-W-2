efb63a3df94e6eb582ad8ae29f18b31af8539f20
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-20 19:20:22 +0500

    up

--- random.js
+++
@@ -1,4 +0,0 @@
-function random(start, stop) {
-    return Math.floor(Math.random()*stop+start)
-}
-module.exports={random}
--- sleep.js
+++
@@ -1,7 +0,0 @@
-function sleep(ms=1000) {
-    return new Promise(res => setTimeout(() => {
-        res(console.log(`Natija: Kod ${ms} ms ishga tushdi`)
-        )
-    },ms))
-}
-module.exports={sleep}
