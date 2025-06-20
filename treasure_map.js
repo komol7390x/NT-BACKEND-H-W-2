4514f99e7fa5cf40e881bcf7c851ac1cc475b3a6
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-20 19:24:23 +0500

    up

--- treasure_map.js
+++ treasure_map.js
@@ -1,23 +1,23 @@
-// const fs = require('fs');
-// const path = require('path');
-// const baseFolder = path.join(__dirname, 'treasure_hunt');
-// const files = {
-//     'map.txt': "Xaritani o'rganing va yo'lingizni toping!",
-//     'clue1.txt': "Siz to'g'ri yo'ldasiz! Lekin quyosh botadigan tomonni unutmang!",
-//     'clue2.txt': "Jangchi yuragiga ega bo'ling! Xazina yaqin!",
-//     'treasure.txt': "Tabriklaymiz! Siz xazinani topdingiz!"
-// };
-// function createTreasureHunt() {
-//     if (!fs.existsSync(baseFolder)) {
-//         fs.mkdirSync(baseFolder);
-//         console.log('Papkasi yaratildi:', baseFolder);
-//     }
-//     for (const [fileName, content] of Object.entries(files)) {
-//         const filePath = path.join(baseFolder, fileName);
-//         fs.writeFileSync(filePath, content, 'utf-8');
-//         console.log(`Fayl yaratildi: ${fileName}`);
-//     }
-//     console.log('Xazina xaritasi tayyor!');
-// }
-// createTreasureHunt();
+const fs = require('fs');
+const path = require('path');
+const baseFolder = path.join(__dirname, 'treasure_hunt');
+const files = {
+    'map.txt': "Xaritani o'rganing va yo'lingizni toping!",
+    'clue1.txt': "Siz to'g'ri yo'ldasiz! Lekin quyosh botadigan tomonni unutmang!",
+    'clue2.txt': "Jangchi yuragiga ega bo'ling! Xazina yaqin!",
+    'treasure.txt': "Tabriklaymiz! Siz xazinani topdingiz!"
+};
+function createTreasureHunt() {
+    if (!fs.existsSync(baseFolder)) {
+        fs.mkdirSync(baseFolder);
+        console.log('Papkasi yaratildi:', baseFolder);
+    }
+    for (const [fileName, content] of Object.entries(files)) {
+        const filePath = path.join(baseFolder, fileName);
+        fs.writeFileSync(filePath, content, 'utf-8');
+        console.log(`Fayl yaratildi: ${fileName}`);
+    }
+    console.log('Xazina xaritasi tayyor!');
+}
+createTreasureHunt();
 
