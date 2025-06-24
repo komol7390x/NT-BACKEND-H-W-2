2a9ddfe90da3161ae5123ab3045e6cc912afd01f
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-24 17:56:56 +0500

    up

--- Asosiy/main5-2.js
+++ Asosiy/main5-2.js
@@ -1,49 +1,79 @@
-const {createServer}=require('node:http');
-const {writeToFile,readFromFile}=require('./file5-2');
+const { createServer } = require('node:http');
+const { writeToFile, readFromFile } = require('./file5-2');
+const { log } = require('node:console');
 
-const server=createServer(async(req,res)=>{
-    const method=req.method
-    const url=req.url
-    if(method=='POST' && url=='/country'){
-        let body=''
-        req.on('data',chunk=>{
-            body+=chunk
+const server = createServer(async (req, res) => {
+    const method = req.method
+    const url = req.url
+    //CREATE
+    if (method == 'POST' && url == '/country') {
+        let body = ''
+        req.on('data', chunk => {
+            body += chunk
         })
-        req.on('end',async()=>{
-            let read=await readFromFile();
-            const data=JSON.parse(body)
-            const newUser={
-                id:!read.length?1:read.at(-1).id+1,...data
+        req.on('end', async () => {
+            let read = await readFromFile();
+            const data = JSON.parse(body)
+            const newUser = {
+                id: !read.length ? 1 : read.at(-1).id + 1, ...data
             }
             read.push(newUser);
             writeToFile(read);
-            res.writeHead(201,{'content-type':'application:json'});
+            res.writeHead(201, { 'content-type': 'application:json' });
             res.end(JSON.stringify({
-                statuCode:201,
-                message:'Done',
-                data:newUser
+                statuCode: 201,
+                message: 'Done',
+                data: newUser
             }))
         })
     }
-
-    if(method=="GET" && url=='/country'){
-        let read=await readFromFile();
-        res.writeHead(200,{'content-type':'application:json'});
-            res.end(JSON.stringify({
-                statuCode:200,
-                message:'Done',
-                data:read
-            }))
+    //READ
+    if (method == "GET" && url == '/country') {
+        let read = await readFromFile();
+        res.writeHead(200, { 'content-type': 'application:json' });
+        res.end(JSON.stringify({
+            statusCode: 200,
+            message: 'Done',
+            data: read
+        }))
     }
+    //UPDATE
+    if (method == "PUT" && url.startsWith('/country/id/')) {
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        })
+        req.on('end', async () => {
+            let read = await readFromFile();
+            id = url.split('/')[3]
+            const index = read.findIndex(res => res.id === +id);
+            if (user == -1) {
+                res.writeHead(404, { 'content-type': 'application:json' });
+                return es.end(JSON.stringify({
+                    statuCode: 404,
+                    message: `Not found Users: ${id}`
+                }))
+            }
+            const updateUser={id:index,...JSON.parse(body)}
+            read[index]=updateUser
+            await writeToFile(read)
+            res.writeHead(200,{ 'content-type': 'application:json' })
+            return res.end(
+                JSON.stringify({
+                    statusCode:200,
+                    message: 'Done',
+                    data: updateUser
+                })
+            )
 
-    if(method=="PUSH" && url=='/country/id/'){
+        })
 
     }
-
-    if(method=='DELETE' && url=='/country/id/'){
+    //DELETE
+    if (method == 'DELETE' && url == '/country/id/') {
 
     }
 
 })
-const PORT=3001
-server.listen(3002,()=>console.log(`Server is running ${PORT} port`))
+const PORT = 3001
+server.listen(3002, () => console.log(`Server is running ${PORT} port`))
