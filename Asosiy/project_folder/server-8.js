284bc28b6a317262651cddfaf2ab55b9cf7dcbe9
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-27 17:23:48 +0500

    up

--- Asosiy/project_folder/server-8.js
+++ Asosiy/project_folder/server-8.js
@@ -1,5 +1,30 @@
 import express from 'express'
-import {read,write} from '../src/crud.js'
+import { read, write } from '../src/crud.js'
+import { error } from 'console';
 
-const app=express();
-console.log(app);
+const app = express();
+// ----------------------------------------------------------------
+//CREATE
+
+app.post('/', async (req, res) => {
+    try {
+        const users = await read();
+        const newUser = {
+            id: !users?.length ? 1 : users.at(-1) + 1, ...req.body
+        }
+        users.push(newUser);
+        await write(users);
+        return res.status(201).json({
+            data: newUser
+        })
+    } catch (error) {
+        if(error){
+            return res.status(500).json({
+                message:error.message|| 'Internal server error'
+            })
+        }
+    }
+})
+
+const PORT=3002
+app.listen(PORT,()=>console.log(`Server is running ${PORT}`))
