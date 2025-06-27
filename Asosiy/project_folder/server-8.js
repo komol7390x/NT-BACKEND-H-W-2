fa7efd115c9b86f4236980dfeca1c96510241398
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-27 20:48:24 +0500

    up

--- Asosiy/project_folder/server-8.js
+++ Asosiy/project_folder/server-8.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { read, write } from '../src/crud.js'
-import { error } from 'console';
 
 const app = express();
 app.use(express.json())
@@ -46,8 +45,17 @@ app.get('/', async (req, res) => {
 //GET BY ID
 app.get('/:id', async (req, res) => {
     try {
-        id = +req.params.id
+        const id = +req.params.id
         const users = await read()
+        const user = users.find(res => res.id == id)
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+        return res.status(200).json({
+            data: user
+        })
 
     } catch (error) {
         if (error) {
@@ -57,5 +65,31 @@ app.get('/:id', async (req, res) => {
         }
     }
 })
+// ----------------------------------------------------------------
+//UPDATE
+app.put('/:id', async (req, res) => {
+    try {
+        const id = +req.params.id;
+        const users = await read();
+        const index = users.findIndex(res => res.id == id);
+        if (index === -1) {
+            res.status(404).json({
+                messag: 'Not found user :('
+            })
+        }
+        users[index] = { id, ...req.body };
+        write(users);
+        return res.status(200).json({
+            messag: users[index]
+        })
+    } catch (error) {
+        if (error) {
+            res.status(500).json({
+                message: error.message || 'Internal server error'
+            })
+        }
+    }
+})
+
 const PORT = 3002
 app.listen(PORT, () => console.log(`Server is running ${PORT}`));
