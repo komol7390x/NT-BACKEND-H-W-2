e243f2c2fe2ad2c3ec2b5b84c7b384dda2f8d2b5
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-29 20:49:18 +0500

    up

---
+++ Qoshimcha/client/app.js
@@ -0,0 +1,29 @@
+const userName = prompt('Ismingizni kiriting: ');
+const socket = new WebSocket('ws://localhost:3003', userName)
+
+const input = document.querySelector("#msg");
+const btn = document.getElementById('btn');
+
+let user = null;
+
+btn.addEventListener('click', () => {
+    const msg = input.value;
+    input.value = ''
+
+    socket.send(JSON.stringify({ msg: msg }))
+})
+
+socket.onopen = function () {
+    console.log('connection to Server')
+}
+
+socket.onmessage = function (event) {
+    const serverMessage = JSON.stringify(event.data);
+    if (!user) {
+        user = serverMessage
+    }
+    if (user.id != serverMessage?.id) {
+        console.log(serverMessage);
+
+    }
+}
---
+++ Qoshimcha/src/server.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 3003 });
+
+const userId = new Set()
+
+wss.on('listening', () => console.log('serveris runing 3003'));
+wss.on('connection', (ws, res) => {
+    console.log('client is connection');
+
+    const userName = res.headers['sec-websocket-protocol'];
+    console.log('bu server', userName);
+
+    const id = Math.random().toString(36).slice(2)
+    userId.add(id);
+    ws.send(JSON.stringify({ id, user: userName }))
+
+    ws.on('message', (data) => {
+        const clientMessage = JSON.stringify(data.toString())
+
+        wss.clients.forEach(client => {
+            client.send(JSON.stringify({ id, user: userName, ...clientMessage }))
+        })
+    })
+})
