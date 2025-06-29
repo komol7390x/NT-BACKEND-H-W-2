eb4c859771b1ac9c0499c31bf324375ce244a0de
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-29 14:43:54 +0500

    up

--- Asosiy/src/controller/user.controller.js
+++ Asosiy/src/controller/user.controller.js
@@ -17,23 +17,83 @@ const addUser = async (req, res) => {
         })
     }
 }
-const getAllUsers = async (req, res) => {
-    const users = await read();
-    return res.status(200).json({
-        data: users
-    })
+const getAllUsers = async (_, res) => {
+    try {
+        const users = await read();
+        return res.status(200).json({
+            data: users
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || 'Internal server error'
+        })
+    }
 }
 
 const getUserById = async (req, res) => {
-
+    try {
+        const users = await read();
+        const id = +req.params.id
+        const user = users.find(item => item.id == id);
+        if (!user) {
+            return res.status(404).json({
+                data: 'not fond Users'
+            })
+        }
+        return res.status(200).json({
+            data: user
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || 'Internal server error'
+        })
+    }
 }
 
 const updateUser = async (req, res) => {
-
+    try {
+        const users = await read();
+        const id = +req.params.id
+        const index = users.findIndex(item => item.id == id);
+        if (index == -1) {
+            return res.status(404).json({
+                data: 'not fond Users'
+            })
+        }
+        users[index] = { id, ...req.body }
+        write(users);
+        return res.status(200).json({
+            message: 'Updata',
+            data: users[index]
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || 'Internal server error'
+        })
+    }
 }
 
 const deleteUser = async (req, res) => {
-
+    try {
+        const users = await read();
+        const id = +req.params.id
+        const index = users.findIndex(item => item.id == id);
+        if (index == -1) {
+            return res.status(404).json({
+                data: 'not fond Users'
+            })
+        }
+        users.splice(index, 1)
+        write(users);
+        return res.status(200).json({
+            message: 'delete',
+            data: {}
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || 'Internal server error'
+        })
+    }
 }
 
 export {
