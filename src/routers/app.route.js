97eff1d05d180d3c92b030a1c1aa0124667f63ad
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-09 15:47:28 +0500

    up

---
+++ src/routers/app.route.js
@@ -0,0 +1,12 @@
+import {Router} from 'express'
+import {createUser,getAllUsers,getByIdUser,updateUser,deleteUser} from '../controller/app.controller.js'
+
+const routers=Router()
+routers
+    .post('/',createUser)
+    .get('/',getAllUsers)
+    .get('/:id',getByIdUser)
+    .put('/:id',updateUser)
+    .delete('/:id',deleteUser)
+
+export default routers
---
+++ src/server-16.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {config} from 'dotenv'
+
+import routers from './routers/app.route.js'
+config()
+
+const server=express();
+server.use(express.json());
+server.use('/data',routers);
+
+const PORT=process.env.PORT
+server.listen(PORT,()=>console.log(`server is running PORT: ${PORT}`))
