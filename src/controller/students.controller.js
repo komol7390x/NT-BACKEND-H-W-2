6353dd70cb0a0349a96ad09d6158e4fa139141fe
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-09 18:01:48 +0500

    up

--- src/controller/students.controller.js
+++ src/controller/students.controller.js
@@ -1,21 +1,48 @@
 import { writeDb, readDb, readDbById, updateDb, deleteDb } from '../helper/students.help.js'
 
 const createUser = async (req, res) => {
-    
+    try {
+        const { full_name, age, guruh_id } = req.body;
+        const result = await writeDb(full_name, age, guruh_id)        
+        if (result.message=='Error') {
+            return res.status(404).json({
+                message: 'Not posted in mysql',
+                data: result
+            })
+        }        
+        if(result.message=='Done'){
+            return res.status(201).json({
+            message: "success",
+            data: result.data
+        })
+        }
+    } catch (error) {
+        if (error) {
+            return res.status(500).json({
+                message: `Server is error ${error.message}`
+            })
+        }
+    }
 }
 
 const getAllUsers = async (_, res) => {
     try {
         const result = await readDb();
+        if (result.message=='Error') {
+            return res.status(404).json({
+                message: 'Not posted in mysql',
+                data: result
+            })
+        } 
         return res.status(200).json({
             statusCode: 200,
             message: 'success',
             data: result
         })
     } catch (error) {
-        if(error){
+        if (error) {
             return res.status(500).json({
-                message:`Server is error ${error.message}`
+                message: `Server is error ${error.message}`
             })
         }
     }
--- src/helper/students.help.js
+++ src/helper/students.help.js
@@ -1,51 +1,76 @@
-import {createConnection} from 'mysql2/promise'
-import {config} from 'dotenv'
+import { createConnection } from 'mysql2/promise'
+import { config } from 'dotenv'
 config()
 
-const connectMysql=async()=>{
-    const mysqlServer=await createConnection({
+const connectMysql = async () => {
+    const mysqlServer = await createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         port: process.env.DB_PORT,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
-        multipleStatements:true
+        multipleStatements: true
     })
     return mysqlServer
 }
 
-const data=await connectMysql()
+const data = await connectMysql()
 
-const writeDb=async()=>{
-
-}
-const readDb=async()=>{
-    const std='SELECT * FROM students'
-    const grh='SELECT * FROM guruh'
-    const [students]=await data.query(std);
-    const [guruh]=await data.query(grh);    
-    const endRes=students.map(item=>{
-        return{
-            id:item.id,
-            name:item.full_name,
-            age:item.age,
-            guruh:guruh.filter(student=>student.id==item.guruh_id)
+const writeDb = async (name, age, guruh) => {
+    try {
+        const std = 'INSERT INTO students(full_name,age,guruh_id) VALUES(?,?,?)'
+        const slt = `SELECT * FROM students WHERE id=?`;
+        const [rows]= await data.query(std, [name, age, guruh]);
+        const result = await data.query(slt, [rows.insertId])            
+        return {
+            message:'Done',
+            data:result[0]
         }
-    })
-    return endRes
+    } catch (error) {
+        if (error) {
+            return {
+                message:'Error',
+                data: `Error database ${error.message}`
+            }
+        }
+    }
 }
-const readDbById=async()=>{
+const readDb = async () => {
+    try {
+        const std = 'SELECT * FROM students'
+        const grh = 'SELECT * FROM guruh'
+        const [students] = await data.query(std);
+        const [guruh] = await data.query(grh);
+        const endRes = students.map(item => {
+            return {
+                id: item.id,
+                name: item.full_name,
+                age: item.age,
+                guruh: guruh.filter(student => student.id == item.guruh_id)
+            }
+        })
+        return endRes
+    } catch (error) {
+        if (error) {
+            return {
+                message:'Error',
+                data: `Error database ${error.message}`
+            }
+        }
+    }
+}
+const readDbById = async () => {
 
 }
 
-const updateDb=async()=>{
+const updateDb = async () => {
 
 }
 
-const deleteDb=async()=>{
+const deleteDb = async () => {
 
 }
 
-export{
-    writeDb,readDb,readDbById,updateDb,deleteDb,connectMysql
+export {
+    writeDb, readDb, readDbById, updateDb, deleteDb, connectMysql
 }
