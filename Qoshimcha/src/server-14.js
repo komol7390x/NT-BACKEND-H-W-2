20485ff328b8585cbbadcfed903a9b3bf6743a48
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-09 15:29:19 +0500

    up

--- Qoshimcha/src/server-14.js
+++
@@ -1,9 +0,0 @@
-import router from '../src/router/app.route.js'
-import express from 'express';
-
-const server = express();
-server.use(express.json());
-server.use('/', router);
-
-const PORT = 3003;
-server.listen(PORT, () => console.log(`Server is running Port:${PORT}`));
