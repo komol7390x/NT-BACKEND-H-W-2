eab3229f99083e17585ed266595535cd00f4588e
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-10 16:11:48 +0500

    up

--- CRUD/src/server-9.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import Router from '../src/routers/user.route.js'
-const app = express();
-app.use(express.json())
-app.use('/user', Router);
-
-const PORT = 3003;
-
-app.listen(PORT, () => console.log(`Server is running ${PORT}`))
