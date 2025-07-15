322b4b220f1b5c04ca98074d1b4ed5767a042a3a
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-15 12:09:35 +0500

    up

--- src/server-20.js
+++ src/server-20.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { config } from 'dotenv';
+import {join} from 'path'
+
 import routerCategory from './routers/category.route.js';
 import routerProduct from './routers/product.route.js';
 import routerReview from './routers/review.route.js'
-
 import { connectDB } from './database/connect.databasa.js';
 config()
 
@@ -15,6 +16,9 @@ server.use(express.static('public'))
 server.use('/category',routerCategory);
 server.use('/product',routerProduct);
 server.use('/review',routerReview);
-server.use('/',)
+
+server.use((_,res)=>{
+    res.status(404).sendFile(join(process.cwd(),'public' ,'image', '404error.png'));
+})
 const PORT=+process.env.PORT
 server.listen(PORT,()=>console.log('Server is runing PORT:',PORT))
